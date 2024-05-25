265292d599e4043704c9ebbf8214bcadfcffbe69
sean-garwood <seansgarwood@gmail.com>
2024-05-25 15:03:58 -0400

    fixup! fixmas: fix 5.5.c

--- ch5/5.5/5.5.c
+++ ch5/5.5/5.5.c
@@ -2,54 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEBUG 1
 #define NARGS 5
 
-int checkargs(int argc, char *argv[], int debug);
+unsigned checkargs(unsigned argc, char *argv[]);
 void info();
-int get_mask(int i, int j);
-int mask_value(int i, int j, int v_0);
-int insert_v_t(int i, int v_t, int *v);
-int or_v0(int v_0, int *v);
-void result(int i, int j, int v_0, int v_t, int v_m);
 
-int get_mask(int i, int j)
+unsigned mask(unsigned i, unsigned j);
+void result(unsigned i, unsigned j, unsigned v_0, unsigned v_t, unsigned v_m);
+
+unsigned mask(unsigned i, unsigned j)
 {
-    int mask = 0;
-    for (int k = i; k <= j; k++)
+    unsigned mask = 0;
+    for (unsigned k = i; k <= j; k++)
         mask |= 1 << k;
     return mask;
 }
 
-int mask_value(int i, int j, int v_0)
+unsigned checkargs(unsigned argc, char *argv[])
 {
-    return (v_0 & get_mask(i, j));
-}
-
-int insert_v_t(int i, int v_t, int *v)
-{
-    return *v | (v_t << i); // shift v_t i bits to the left and OR with
-}
-
-int or_v0(int v_0, int *v)
-{
-    return (*v | v_0);
-}
-
-int checkargs(int argc, char *argv[], int debug)
-{
-    if (debug)
+    if (DEBUG)
     {
+
+        argv[1] = 15;
+        argv[2] = 0;
+        argv[3] = 0;
+        argv[4] = 3;
+
         printf("============-DEBUG MODE-============\n");
-
-        argv[1] = "15";
-        argv[2] = "0";
-        argv[3] = "0";
-        argv[4] = "3";
-
-        printf("argv[1]: v_0 set to %s\n", argv[1]);
-        printf("argv[2]: v_t set to %s\n", argv[2]);
-        printf("argv[3]: i set to %s\n", argv[3]);
-        printf("argv[4]: j set to %s\n", argv[4]);
+        printf("argv[1]: v_0 set to %d\n", argv[1]);
+        printf("argv[2]: v_t set to %d\n", argv[2]);
+        printf("argv[3]: i set to %d\n", argv[3]);
+        printf("argv[4]: j set to %d\n", argv[4]);
     }
     else if (argc != NARGS)
     {
@@ -80,7 +64,7 @@ void info()
     printf("    j: ending bit\n");
 }
 
-void result(int v_0, int v_t, int i, int j, int v_m)
+void result(unsigned v_0, unsigned v_t, unsigned i, unsigned j, unsigned v_m)
 {
     printf("v_0: %d\n", v_0);
     printf("v_t: %d\n", v_t);
@@ -89,7 +73,7 @@ void result(int v_0, int v_t, int i, int j, int v_m)
     printf("after shifting v_t%d bits left: %d\n", i, v_t);
     printf("    v_m: %d\n", v_m);
     printf("    v_m in binary: ");
-    for (int k = 31; k >= 0; k--)
+    for (unsigned k = 31; k >= 0; k--)
     {
 
         printf("%d", (v_m >> k) & 1);
@@ -98,21 +82,21 @@ void result(int v_0, int v_t, int i, int j, int v_m)
     }
 }
 
-int main(int argc, char *argv[])
+int main(unsigned argc, char *argv[])
 {
-    int debug = 1;
-    if (checkargs(argc, argv, debug))
+    if (checkargs(argc, *argv))
         return 1;
 
-    int v_0 = atoi(argv[1]);
-    int v_t = atoi(argv[2]);
-    int i = atoi(argv[3]);
-    int j = atoi(argv[4]);
+    unsigned v_0 = atoi(argv[1]);
+    unsigned v_t = atoi(argv[2]);
+    unsigned i = atoi(argv[3]);
+    unsigned j = atoi(argv[4]);
 
-    int v_m = mask_value(i, j, v_0);
+    unsigned v_m;
+    v_m = v_0 & ~mask(i, j);
     printf("v_m: %d\n", v_m);
-    v_m = insert_v_t(i, v_t, &v_m);
-    v_m = or_v0(v_0, &v_m);
+    v_m |= (v_t << i); // shift v_t i bits to the left and OR with
+    v_m |= v_0;
     result(v_0, v_t, i, j, v_m);
     return 0;
 }
