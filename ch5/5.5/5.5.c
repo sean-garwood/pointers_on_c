f0b0f075992980915a9f94a54c538d266e926409
sean-garwood <seansgarwood@gmail.com>
2024-05-24 23:51:49 -0400

    fix: somewhat functional
    * failed 255, 0, 4, 7 #=> 255, expected 15

--- ch5/5.5/5.5.c
+++ ch5/5.5/5.5.c
@@ -1,85 +1,85 @@
 /* write a fn that will store a given value into specified bit positions of an int.*/
 #include <stdio.h>
 #include <stdlib.h>
-#define ARGS 5
 
-short get_mask(const __uint16_t starting_bit, const __uint16_t ending_bit);
-void zero_bits(short original_value, short mask);
-/*it should have the prototype*/
-short store_bit_field(short original_value, short value_to_store,
-                      const __uint16_t starting_bit, const __uint16_t ending_bit,
-                      short mask);
-/*  note: adjusted for 1999 status and to fit the examples below.
+#define NARGS 5
 
+int checkargs(int argc);
+void info();
+int get_mask(int i, int j);
+int mask_value(int i, int j, int v_0);
+int insert_v_t(int i, int v_t, int *v);
+int or_v0(int v_0, int *v);
+void result(int i, int j, int v_0, int v_t, int v_m);
 
-some example outputs:
-
-    store_bit_field(0x0, 0x1, 4, 4) -> 0x10
-    store_bit_field(0xffff, 0x123, 15, 4) -> 0x123f
-    store_bit_field(0xffff, 0x123, 13, 9) -> 0xc7ff
-
-some tips:
-
-    1. construct a mask that contains ones in the bit positions that correspond to
-    the desired field.
-
-    2. Using one's complement of the mask, clear all of the bits in the field.
-
-    3. Shift the value to store so that it aligns with the field.
-
-    4. AND the shifted value with the mask to ensure that only the desired bits are
-    set.
-
-    5. OR the result with the original value to set the desired bits.*/
-
-short get_mask(const __uint16_t starting_bit, const __uint16_t ending_bit)
+int get_mask(int i, int j)
 {
-    short mask = 0;
-
-    /* set the bits from starting_bit to ending_bit to 1
-    keep the rest zero
-
-    keep in mind that starting_bit is the most significant bit
-    Assume bits in int are numbered from right to left, and that starting bit
-    position cannot be less than ending bit position.*/
-
-    for (int i = ending_bit; i <= starting_bit; i++)
-    {
-        mask |= 1 << i;
-    }
+    int mask = 0;
+    for (int k = i; k <= j; k++)
+        mask |= 1 << k;
     return mask;
 }
 
-void zero_bits(short original_value, short mask)
+int mask_value(int i, int j, int v_0)
 {
-    original_value &= ~mask;
+    return (v_0 & get_mask(i, j));
 }
 
-short store_bit_field(short original_value, short value_to_store,
-                      const __uint16_t starting_bit, const __uint16_t ending_bit,
-                      short mask)
+int insert_v_t(int i, int v_t, int *v)
 {
-    original_value |= (value_to_store << starting_bit) & mask;
-    return original_value;
+    return *v | (v_t << i); // shift v_t i bits to the left and OR with
+}
+
+int or_v0(int v_0, int *v)
+{
+    return (*v | v_0);
+}
+
+int checkargs(int argc)
+{
+    argc != NARGS ? info() : 0;
+    return 0;
+}
+
+void info()
+{
+    printf("Usage: ./5.5 <v_0> <v_t> <i> <j>\n");
+    printf("    v_0: original value\n");
+    printf("    v_t: value to insert\n");
+    printf("    i: starting bit\n");
+    printf("    j: ending bit\n");
+}
+
+void result(int v_0, int v_t, int i, int j, int v_m)
+{
+    printf("v_0: %d\n", v_0);
+    printf("v_t: %d\n", v_t);
+    printf("i: %d\n", i);
+    printf("j: %d\n", j);
+    printf("after shifting v_t%d bits left: %d\n", i, v_t);
+    printf("    v_m: %d\n", v_m);
+    printf("    v_m in binary: ");
+    for (int k = 31; k >= 0; k--)
+    {
+
+        printf("%d", (v_m >> k) & 1);
+        k == 0 ? printf("\n") : 0;
+        k % 4 == 0 ? printf(" ") : 0;
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc != ARGS)
-    {
-        printf("Usage: %s <original_value> <value_to_store> <starting_bit> <ending_bit>\n", argv[0]);
-        return 1;
-    }
-    short val = atoi(argv[1]);
-    short val_to_store = atoi(argv[2]);
-    const __uint16_t start_bit = atoi(argv[3]);
-    const __uint16_t end_bit = atoi(argv[4]);
+    if (checkargs(argc)) return 1;
 
-    short mask = get_mask(start_bit, end_bit);
-    zero_bits(val, mask);
+    int v_0 = atoi(argv[1]);
+    int v_t = atoi(argv[2]);
+    int i = atoi(argv[3]);
+    int j = atoi(argv[4]);
 
-    short result = store_bit_field(val, val_to_store, start_bit, end_bit, mask);
-    printf("changing the %hu-%hu bits of %hd to %hd yields %hd\n",
-           start_bit, end_bit, val, val_to_store, result);
+    int v_m = mask_value(i, j, v_0);
+    v_m = insert_v_t(i, v_t, &v_m);
+    v_m = or_v0(v_0, &v_m);
+    result(v_0, v_t, i, j, v_m);
     return 0;
 }
