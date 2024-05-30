978b8e84110ab70f7d0baa06df966d1b0ad83f80
sean-garwood <seansgarwood@gmail.com>
2024-05-30 15:08:26 -0400

    fixup! fixup!

--- ch7/6/6.c
+++ ch7/6/6.c
@@ -15,7 +15,21 @@ int reverse(int dollars, int *rev_digits);
 int reverse(int dollars, int *buffer);
 void print_num(int dollars, char *buffer);
 
-// write the digits to an int array in reverse order. return the number of digits
+/*
+ * declare some arrays of constant character pointers to map numbers to words*/
+const char *ones[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+const char *teens[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                       "eighteen", "nineteen"};
+const char *tees[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+                      "eighty", "ninety"};
+const char *hundreds[] = {"", "one hundred", "two hundred", "three hundred", "four hundred",
+                          "five hundred", "six hundred", "seven hundred", "eight hundred",
+                          "nine hundred"};
+const char *powers[] = {"", "thousand", "million", "billion", "trillion"};
+
+/*
+ * write the digits to an int array in reverse order. return the number of
+ * digits */
 int reverse(int dollars, int *buffer)
 {
     int i = 0;
@@ -30,24 +44,41 @@ int reverse(int dollars, int *buffer)
 
 void print_num(int dollars, char *buffer)
 {
-    /*
-     * declare some arrays of character pointers to map numbers to words    */
-
-    char *ones[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    char *teens[] = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
-                     "eighteen", "nineteen"};
-    char *tens[] = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy",
-                    "eighty", "ninety"};
-    char *hundreds[] = {"", "one hundred", "two hundred", "three hundred", "four hundred",
-                        "five hundred", "six hundred", "seven hundred", "eight hundred",
-                        "nine hundred"};
-    char *powers[] = {"", "thousand", "million", "billion", "trillion"};
-    int index, power, length, thousands_index;
     int rev_digits[MAX_POWER]; // store the digits of the number in reverse order
+    int digits = reverse(dollars, rev_digits);
+    // now the buffer is distributed into three-digit chunks
+    int power = 0;
+    for (int i = 0; i <= digits; i += 3)
+    {
+        int chunk = rev_digits[i] + 10 * rev_digits[i + 1] + 100 * rev_digits[i + 2];
+        if (chunk > 0)
+        {
+            if (buffer)
+            {
+                strcat(buffer, hundreds[rev_digits[i + 2]]);
+                if (rev_digits[i + 1] == 1)
+                {
+                    strcat(buffer, teens[rev_digits[i]]);
+                }
+                else
+                {
+                    strcat(buffer, tees[rev_digits[i + 1]]);
+                    strcat(buffer, ones[rev_digits[i]]);
+                }
+                strcat(buffer, powers[power]);
+            }
+            else
+            {
+                printf("%s %s %s %s\n", hundreds[rev_digits[i + 2]], rev_digits[i + 1] == 1 ? teens[rev_digits[i]] : tees[rev_digits[i + 1]], ones[rev_digits[i]], powers[power]);
+            }
+        }
+        power++;
+    }
 }
 
 int main(int argc, char *argv[])
 {
+    char buffer[MAX_POWER];
     // if no args, prompt user for input
     if (argc < 2)
     {
