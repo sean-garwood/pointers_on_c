6a7cffa46fc7fd008e7c863ea42f3464e5c9a7cb
sean-garwood <seansgarwood@gmail.com>
2024-05-30 14:41:12 -0400

    fixup!
    * define reverse:
      * takes an int and a pointer to an int array
        that stores the reversed digits from dollars.
    * add cli args
    * write out main
    
    chore: add .vscode/ to .gitignore

--- ch7/6/6.c
+++ ch7/6/6.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
+#define MAX_POWER 15
 // chop off least significant digits and add them to buffer
-void build_reversed(unsigned int dollars, char *buffer);
+int reverse(int dollars, int *rev_digits);
 // the length of the number can be derived from this function, as well.
 
 /* the objective is to print the number out in english words.
@@ -12,20 +12,23 @@ void build_reversed(unsigned int dollars, char *buffer);
  *    example: 123456789 -> one hundred twenty three million four hundred fifty
  *    six thousand seven hundred eighty nine
  */
-void print_num(unsigned int dollars, char *buffer);
+int reverse(int dollars, int *buffer);
+void print_num(int dollars, char *buffer);
 
-void build_reversed(unsigned int dollars, char *buffer)
+// write the digits to an int array in reverse order. return the number of digits
+int reverse(int dollars, int *buffer)
 {
     int i = 0;
     while (dollars > 0)
     {
-        buffer[i++] = dollars % 10 + '0';
+        buffer[i++] = dollars % 10;
         dollars /= 10;
     }
-    buffer[i] = '\0';
+    buffer[i] = -1;
+    return i - 1; // to be used in the print_num function
 }
 
-void print_num(unsigned int dollars, char *buffer)
+void print_num(int dollars, char *buffer)
 {
     /*
      * declare some arrays of character pointers to map numbers to words    */
@@ -39,16 +42,24 @@ void print_num(unsigned int dollars, char *buffer)
                         "five hundred", "six hundred", "seven hundred", "eight hundred",
                         "nine hundred"};
     char *powers[] = {"", "thousand", "million", "billion", "trillion"};
-    int digits, index, power, length, thousands_index;
-
-    build_reversed(dollars, buffer);
-    length = strlen(buffer);
-    power = (length - 2);
-    thousands_index = 0;
+    int index, power, length, thousands_index;
+    int rev_digits[MAX_POWER]; // store the digits of the number in reverse order
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-
+    // if no args, prompt user for input
+    if (argc < 2)
+    {
+        int dollars;
+        printf("Enter a dollar amount: ");
+        scanf("%u", &dollars);
+        print_num(dollars, NULL);
+    }
+    else
+    {
+        int dollars = atoi(argv[1]);
+        print_num(dollars, NULL);
+    }
     return 0;
 }
