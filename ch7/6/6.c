4ba26ae9cae517c08c8f01c146cf6d0f1a3cb893
sean-garwood <seansgarwood@gmail.com>
2024-05-31 14:25:27 -0400

    feat: solve 7.6

--- ch7/6/6.c
+++ ch7/6/6.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 #define MAX_POWER 15
-int reverse(int dollars, int *rev_digits);
+int num_digits(int dollars);
+int tent_to_the(int power);
 void print_num(int dollars, char *buffer);
 
 /* declare some arrays of constant character pointers to map numbers to words*/
@@ -17,20 +18,6 @@ const char *hundreds[] = {"", "one hundred ", "two hundred ", "three hundred ", 
                           "nine hundred "};
 const char *powers[] = {"", "thousand ", "million ", "billion ", "trillion "};
 
-/*
- * write the digits to an int array in reverse order. return the number of
- * digits */
-int reverse(int dollars, int *buffer)
-{
-    int digits = 0;
-    while (dollars > 0)
-    {
-        buffer[digits++] = dollars % 10;
-        dollars /= 10;
-    }
-    return digits;
-}
-
 int num_digits(int dollars)
 {
     int digits = 0;
@@ -42,60 +29,49 @@ int num_digits(int dollars)
     return digits;
 }
 
-// void print_num(int dollars, char *buffer)
-// {
-//     int rev_digits[MAX_POWER]; // store the digits of the number (big endian)
-//     int digits = reverse(dollars, rev_digits);
-//     const char **power = powers;
-
-//     for (int index = 0; index < digits; index += 3)
-//     {
-//         int o = rev_digits[index];
-//         int t = index + 1 < digits ? rev_digits[index + 1] : 0;
-//         int h = index + 2 < digits ? rev_digits[index + 2] : 0;
-//         int is_more = index + 3 < digits;
-//         // initialize a pointer to address of digits[index + 2]
-//         int chunk[3] = {h, t, o};
-//         int chunk_index = index % 3;
-//         printf("Chunk: %d %d %d\n", h, t, o);
-
-//         is_more ? strcat(buffer, *++power) : 0;
-//         strcat(buffer, hundreds[h]);
-//         if (t == 1)
-//         {
-//             strcat(buffer, teens[o]);
-//             continue;
-//         }
-//         else
-//             strcat(buffer, tees[t]);
-//         strcat(buffer, ones[o]);
-//     }
-// }
-void print_num(int dollars, char *buffer)
+// calculate the number to divide dollars by when creating chunk
+int ten_to_the(int power)
 {
-    if (dollars == 0)
-        return;
-
-    int last_three_digits = dollars % 1000;
-    print_num(dollars / 1000, buffer);
-
-    int h = last_three_digits / 100;
-    int t = (last_three_digits % 100) / 10;
-    int o = last_three_digits % 10;
-
-    strcat(buffer, hundreds[h]);
-    if (t == 1)
+    if (!power)
     {
-        strcat(buffer, teens[o]);
+        return 1;
     }
     else
     {
-        strcat(buffer, tees[t]);
-        strcat(buffer, ones[o]);
+        int p = 1;
+        for (int i = 0; i < power; i++)
+        {
+            p *= 1000;
+        }
+        return p;
     }
-    if ((dollars / 1000) > 0)
+}
+
+void print_num(int dollars, char *buffer)
+{
+    int power = num_digits(dollars) / 3;
+    while (power >= 0)
     {
-        strcat(buffer, powers[num_digits(dollars) / 3]);
+        // first group of dollars.
+        int chunk, o, t, h;
+        chunk = power ? dollars / ten_to_the(power) : dollars % 1000;
+        o = chunk % 10;         // ones
+        t = (chunk / 10) % 10;  // tens
+        h = (chunk / 100) % 10; // hundreds
+
+        strcat(buffer, hundreds[h]);
+        if (t == 1)
+        {
+            strcat(buffer, teens[o]);
+        }
+        else
+        {
+            strcat(buffer, tees[t]);
+            strcat(buffer, ones[o]);
+        }
+        strcat(buffer, powers[power]);
+
+        power--;
     }
 }
 
@@ -108,14 +84,12 @@ int main(int argc, char *argv[])
     {
         printf("Enter a dollar amount: ");
         scanf("%u", &dollars);
-        print_num(dollars, buffer);
     }
     else
     {
         dollars = atoi(argv[1]);
-        printf("Dollars: %d\n", dollars);
-        print_num(dollars, buffer);
     }
+    print_num(dollars, buffer);
     printf("%s\n", buffer);
     return 0;
 }
