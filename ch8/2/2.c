e1a579deaa6f0045d32d5578d868fddf7c73eb7a
sean-garwood <seansgarwood@gmail.com>
2024-06-06 11:37:47 -0400

    feat: solve 8.2

--- ch8/2/2.c
+++ ch8/2/2.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
-#define CAPS                                      \
-    {                                             \
-        0.0, 23350.0, 56550.0, 117950.0, 256500.0 \
-    }
-#define RATES                         \
-    {                                 \
-        0.15, 0.28, 0.31, 0.36, 0.396 \
-    }
-#define FLAT                                  \
-    {                                         \
-        3502.50, 12798.50, 31832.50, 81710.50 \
-    }
 #define BRACKETS 5
-
 /*
  * the us fed income tax for single ppl in 1995 was computed thus:
 
@@ -32,6 +19,7 @@ float tax_due(float income);
 
 int main(void)
 {
+    // get income
     float income;
     printf("Enter taxable income: ");
     scanf("%f", &income);
@@ -41,28 +29,23 @@ int main(void)
 
 float tax_due(float income)
 {
+    const float rates[][3] = {
+        {0, 0, 0.15}, // flat rate, floor, rate
+        {23350, 3502.5, 0.28},
+        {56550, 12798.5, 0.31},
+        {117950, 31832.5, 0.36},
+        {256500, 81710.5, 0.396}};
+    const int num_brackets = sizeof(rates) / sizeof(rates[0]);
+    float tax = 0.0;
 
-    //  there are five tax brackets. each has:
-    //      lo range
-    //      hi range
-    //      an offset
-    //      percentage
-    //      cutoff = low range of your bracket
-    //  in the spirit of the subject matter, we will use a multidimensional array to
-    //  represent this data structure.
-
-    // this has been done in the #define statements above.
-    // let's use pointers to these brackets and a loop to find the index we
-    // should move to in the brackets array.
-    const float caps[BRACKETS] = CAPS;
-    const float rates[BRACKETS] = RATES;
-    const float flat[BRACKETS] = FLAT;
-
-    for (int b = 0; b < BRACKETS; b++)
+    for (int i = num_brackets - 1; i >= 0; i--)
     {
-        if (income < *(caps + b))
+        if (income > rates[i][0])
         {
-            return *(flat + b) + *(rates + b) * (income - *(caps + b - 1));
+            tax = rates[i][1] + (income - rates[i][0]) * rates[i][2];
+            break;
         }
     }
+
+    return tax;
 }
