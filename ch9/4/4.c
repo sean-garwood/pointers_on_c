1b9fc3f7018ae2e3eaee037552e8cdc7133756a7
sean-garwood <seansgarwood@gmail.com>
2024-06-26 10:31:46 -0400

    chore: 9.4

---
+++ ch9/4/4.c
@@ -0,0 +1,43 @@
+/*
+ * write a fn `my_strcat` that is similar to strcat but will not overflow the
+ * destination array.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 10
+
+char *my_strcat(char *dest, const char *src, size_t len);
+
+int main(void)
+{
+    // dummy values
+    char dest[SIZE];
+    const char src[] = "foo";
+    for (int i = 0; i < SIZE; i++)
+    {
+        dest[i] = 'a';
+    }
+    my_strcat(dest, src, 8);
+    printf("dest: %s\n", dest);
+    return 0;
+}
+
+char *my_strcat(char *dest, const char *src, size_t len)
+{
+    if (len > SIZE)
+        len = SIZE;
+    else if (len + strlen(dest) > SIZE)
+    {
+        /* overwrite the last len bytes of dest with the first len
+         * bytes of src if the length of dest + src is greater than
+         * the size of dest.
+         */
+        char *overwrite = dest + strlen(dest) - len;
+        strncpy(overwrite, src, len);
+    }
+    else
+        strncat(dest, src, len);
+    return dest;
+}
