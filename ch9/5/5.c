d3a6db3eb85875ef78679206c060f54b2bccbf99
sean-garwood <seansgarwood@gmail.com>
2024-06-26 11:56:34 -0400

    chore: solve 9.5

---
+++ ch9/5/5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 10
+
+// write a function:
+void my_strncat(char *dest, char *src, int dest_len);
+
+/*
+ * it should:
+ * concat the str in src to the end of the str in dest
+ * does not overflow dest, which has a len of dest_len
+ * always null-terminate dest
+ */
+
+int main(void)
+{
+    char dest[SIZE] = "foo";
+    char src[] = "barbazbat";
+    int dest_len = strlen(dest);
+    // cat src to dest
+    my_strncat(dest, src, dest_len);
+    // print result
+    printf("%s\n", dest);
+    return 0;
+}
+
+void my_strncat(char *dest, char *src, int dest_len)
+{
+    for (int i = 0; i < SIZE - dest_len - 1; i++)
+        dest[dest_len + i] = src[i];
+    dest[SIZE - 1] = '\0';
+}
